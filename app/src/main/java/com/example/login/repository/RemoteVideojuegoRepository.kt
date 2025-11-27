9caea81507198bed94cf6dad283f2c4074651555
ignacioruizberenguela <ig.ruizb@duocuc.cl>
2025-11-27 20:40:05 -0300

    Add obtenerVideojuegosRawg method to repository

--- app/src/main/java/com/example/login/repository/RemoteVideojuegoRepository.kt
+++ app/src/main/java/com/example/login/repository/RemoteVideojuegoRepository.kt
@@ -4,5 +4,8 @@ import com.example.login.model.Videojuego
 import com.example.login.network.ApiService
 
 class RemoteVideojuegoRepository(private val api: ApiService) {
-    suspend fun obtenerVideojuegos(): List<Videojuego> = api.obtenerVideojuegos()
+    suspend fun obtenerVideojuegosRawg(apiKey: String, pageSize: Int = 20): List<Videojuego> {
+        val resp = api.getGames(apiKey, pageSize)
+        return resp.toVideojuegos()
+    }
 }
